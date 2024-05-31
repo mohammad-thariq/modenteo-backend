ff9d59935832bfa83c79bb9cd2cb2a14042ae07b
Gomathy <gomathy1023@gmail.com>
2024-05-31 12:08:10 +0530

    Menu routings

--- src/models/Menu/menu.controller.js
+++ src/models/Menu/menu.controller.js
@@ -1,5 +1,5 @@
 import {
-  getCategoriesWithSubcategory,
+  getCategoriesWithSubcategory,getSubcategorywithChildCategories
 } from "./menu.service.js";
 
 export const getCategoryWithSubcategory = (req, res) => {
@@ -31,6 +31,47 @@ export const getCategoryWithSubcategory = (req, res) => {
               id: cat?.subcategoryID,
               name: cat?.subcatName,
               slug: cat?.subcatSlug,
+            },
+          ],
+        };
+      }
+    }
+    const responseArray = Object.values(response);
+    return res.status(200).json({
+      response: responseArray,
+    });
+  });
+};
+
+export const getSubcategoryWithChildcategory = (req, res) => {
+  getSubcategorywithChildCategories(async (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        error: "Database connection error",
+      });
+    }
+    let response = {};
+
+    for (const subcat of results) {
+      if (response[subcat?.id]) {
+        response[subcat?.id].childCategory.push({
+          id: subcat?.childcatID,
+          name: subcat?.childcatName,
+          slug: subcat?.childcatSlug,
+        });
+      } else {
+        response[subcat?.id] = {
+          id: subcat?.id,
+          categoryName: subcat?.name,
+          categorySlug: subcat?.slug,
+          image: subcat?.image,
+          isOpen: false,
+          childCategory: [
+            {
+              id: subcat?.childcatID,
+              name: subcat?.childcatName,
+              slug: subcat?.childcatSlug,
             },
           ],
         };
--- src/models/Menu/menu.router.js
+++ src/models/Menu/menu.router.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {
   getCategoryWithSubcategory,
+  getSubcategoryWithChildcategory,
 } from "./menu.controller.js";
 
 const router = express.Router();
 
-router.get("/menu/categories",getCategoryWithSubcategory);
+router.get("/menu/categories/sub-categories",getCategoryWithSubcategory);
+router.get("/menu/sub-categories/child-categories",getSubcategoryWithChildcategory);
 
 export default router;
