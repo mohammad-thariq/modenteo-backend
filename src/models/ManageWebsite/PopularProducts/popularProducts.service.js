09384f0ed36b29d8720f10e6285f6cd9da44a537
Gomathy <gomathy1023@gmail.com>
2024-06-10 19:57:52 +0530

    Popular Products

--- src/models/ManageWebsite/PopularProducts/popularProducts.service.js
+++ src/models/ManageWebsite/PopularProducts/popularProducts.service.js
@@ -2,11 +2,10 @@ import db from "../../../database/index.js";
 
 export const create = (data, callBack) => {
   db.query(
-    `INSERT INTO popular_product (title, description, button_name, page_url, status, image) VALUES (?, ?, ?, ?, ?, ?)`,
+    `INSERT INTO popular_product (title, description,  page_url, status, image) VALUES (?, ?, ?, ?, ?)`,
     [
       data.title,
       data.description,
-      data.button_name,
       data.page_url,
       data.status,
       data.image,
@@ -22,7 +21,7 @@ export const create = (data, callBack) => {
 
 export const getByPopularProductId = (id, callBack) => {
   db.query(
-    `SELECT id, title, sub_title, description, button_name, page_url, status, image from popular_product WHERE id = ?`,
+    `SELECT id, title, sub_title, description,  page_url, status, image from popular_product WHERE id = ?`,
     [id],
     (error, results) => {
       if (error) {
@@ -35,7 +34,7 @@ export const getByPopularProductId = (id, callBack) => {
 
 export const getPopularProduct = (callBack) => {
   db.query(
-    `SELECT id, title, description, button_name, page_url, status, image FROM popular_product`,
+    `SELECT id, title, description,  page_url, status, image FROM popular_product`,
     [],
     (error, results) => {
       if (error) {
@@ -48,11 +47,10 @@ export const getPopularProduct = (callBack) => {
 
 export const updatePopularProduct = (data, id, callBack) => {
   db.query(
-    `UPDATE popular_product SET title = ?, description = ?, button_name = ?, page_url = ?, status = ?, image = ? WHERE id = ?`,
+    `UPDATE popular_product SET title = ?, description = ?,  page_url = ?, status = ?, image = ? WHERE id = ?`,
     [
       data.title,
       data.description,
-      data.button_name,
       data.page_url,
       data.status,
       data.image,
